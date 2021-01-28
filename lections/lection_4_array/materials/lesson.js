b2104c172ec7f2f624a88aaf6d0de7fa3109be1a
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2021-01-28 22:15:45 +0200

    l-4

--- lections/lection_4_array/materials/lesson.js
+++ lections/lection_4_array/materials/lesson.js
@@ -3,3 +3,62 @@
     * Двумерные массивы
     * Зубчатые массива\ Ступенчатые массивы
 */
+
+var rowFirst = [1, 2, 3];
+var rowSecond = [4, 5, 6];
+
+// var twoDem = [
+//   rowFirst,
+//   rowSecond
+// ];
+
+var twoDem = [
+  [1, 2, 3],
+  [4, 5, 6]
+];
+
+/*
+    [
+      [1, 2, 3], 
+      [4, 5, 6]
+    ]
+
+*/
+
+console.log(twoDem, 'twoDem');
+// var row = 0;
+// var column = 0;
+// console.log(twoDem[row][column], '0')
+// console.log(twoDem[1][2], 'row = 1, column = 2')
+
+// for(var i = 0, sizeI = twoDem.length; i < sizeI; i++) {
+//     // console.log(twoDem[i], 'i == ' + i);
+//     var innerArray = twoDem[i];
+
+//     for(var j = 0, sizeJ = innerArray.length; j < sizeJ; j++) {
+    
+//       console.log(innerArray[j], 'row => ' + i + ' ' + "column => " + j)
+//     }
+
+
+// }
+
+var size = 5;
+var mass = [];
+
+mass.length = size;
+
+
+for(var row = 0; row < mass.length; row++) {
+  mass[row] = [];
+  mass[row].length = size;
+
+
+  for(var column = 0; column < mass[row].length; column++) {
+      // console.log(mass[row][column], 'row | column');
+      mass[row][column] = Math.round(Math.random() * 60);
+  }
+}
+
+
+console.log(mass, 'mass');
--- lections/lection_4_array/materials/practice-array.js
+++ lections/lection_4_array/materials/practice-array.js
@@ -1,17 +1,67 @@
 
-// ------ practice HW(3.3) (min, max) -----
-
-
-
-
-// ----- practice HW(3.4) (revert) ------
-
-
-
 // ------ practice (two-dimensional array) ------ 
 
 
+/*
+
+1)  В двумерном массиве A размером n(рядов) на m(столбцов). Заполнить случайными числами.
+    1. Найти ряд, где сумма элементов наименьшая
+    2. Найти ряд, где сумма элементов найбольшая
+    3. Поменять ряды местами (1 и 2 пункт)
+
+*/
+var size = 5;
+var mass = [];
+mass.length = 5;
+
+for(var row = 0; row < mass.length; row++) {
+    mass[row] = [];
+    mass[row].length = size;
+  
+    for(var column = 0; column < mass[row].length; column++) {
+        mass[row][column] = Math.round(Math.random() * 60);
+    }
+}
+
+console.log(mass, 'mass');
+
+var minRow;
+var maxRow;
+
+var minRowI;
+var maxRowI;
+
+for (var i = 0; i < mass.length; i++) {
+    for(var j = 0, sum = 0; j < mass[i].length; j++) {
+        sum += mass[i][j]; 
+    }
+    console.log(i + ' row => ' + sum)
+
+    if (i === 0) {
+        minRow = sum;
+        maxRow = sum;
+        // continue;
+    }
+
+    //afte 
+    if (sum >= maxRow) {
+        maxRow = sum;
+        maxRowI = i;
+    }
+
+    if (minRow >= sum) {
+        minRow = sum;
+        minRowI = i;
+    } 
+
+}
 
 
+console.log(minRow, maxRow, 'minRow, maxRow');
+console.log(minRowI, maxRowI, 'minRowI, maxRowI');
 
+var swap = mass[minRowI];
+mass[minRowI] = mass[maxRowI];
+mass[maxRowI] = swap;
 
+console.log(mass, 'mass after');
